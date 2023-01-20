0bc4e6122ddfa0529135395067859bdb33c8e6f0
KUMA <KUMA@DESKTOP-SRIGRTP>
2023-01-20 16:52:58 +0800

    fix: 取消arucoDetection使用多執行續 每個細項做的事太少，以致無法達到多執行續效益，且分配過多任務導致花費太多時間在分配任務

--- aruco_locating/src/aruco_detection.cpp
+++ aruco_locating/src/aruco_detection.cpp
@@ -9,29 +9,20 @@ void ArucoDetection::detect(const cv::Mat & image, Markers& markers) {
 	std::vector<cv::Vec4i> hierarchy;
 	cv::adaptiveThreshold(gray, thresholded, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, Settings::adaptiveThreshBlockSize, Settings::adaptiveThreshC);
 	cv::findContours(thresholded, candidates, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-#pragma omp parallel
-	{
-		std::vector<Marker> markers_private;
-	#pragma omp for nowait
-		for (int i = 0; i < candidates.size(); i++) {
-			Poly* candidate = &candidates.at(i);
-			if (!polyfilter(*candidate,hierarchy, i)) continue;
-			PolyF candidatef;
-			cv::Mat warped;
-			std::unique_ptr<Marker> markers;
-			makePointsOrder(*candidate);
-			cornerRefine(*candidate, candidatef);
-			warp(candidatef, warped);
-			MarkerId id = -1;
-			if ((id=idMatcher(warped, candidatef))==-1) continue;
-			Marker marker(id, Params::dictionary.markerSize(id), candidatef);
-			cv::undistortPoints(marker.corners, marker.corners, Params::kmat, Params::distmat,cv::noArray(),Params::kmat);
-			markers_private.emplace_back(marker);
-		}
-	#pragma omp critical
-		for (const Marker& marker : markers_private) {
-			markers.emplace(marker.id, marker);
-		}
+	markers.clear();
+	int i = 0;
+	for (auto& candidate:candidates) {
+		if (!polyfilter(candidate,hierarchy, i++)) continue;
+		PolyF candidatef;
+		cv::Mat warped;
+		makePointsOrder(candidate);
+		cornerRefine(candidate, candidatef);
+		warp(candidatef, warped);
+		MarkerId id = -1;
+		if ((id=idMatcher(warped, candidatef))==-1) continue;
+		Marker marker(id, Params::dictionary.markerSize(id), candidatef);
+		cv::undistortPoints(marker.corners, marker.corners, Params::kmat, Params::distmat,cv::noArray(),Params::kmat);
+		markers.emplace(marker.id, marker);
 	}
 }
 
