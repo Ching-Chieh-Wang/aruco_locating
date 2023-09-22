8056198e92ce2088ce8dc13e740764eddc1be3cc
John MacTavish <wangisme@yahoo.com.tw>
2023-09-22 16:44:56 +0800

    Chore: align main function to github respository page folder sturcture

--- aruco_locating/test/main.cpp
+++ aruco_locating/test/main.cpp
@@ -1,13 +1,12 @@
 ﻿#include "pch.h"
 #include "aruco_locating.h"
 
-
-
 int main(){
 // key: 照片資料夾路徑,value:.json檔的路徑
 	ArucoLocating arucoLocator;
-	arucoLocator.run(Source::IMAGE, "imgs", true,false, true);
-	arucoLocator.save();
+	arucoLocator.arucoDetectionSet(Source::IMAGE, "asset/imgs");
+	arucoLocator.run(Source::IMAGE, "asset/imgs", true,true, false);
+	arucoLocator.save("asset/saved");
 }
 
 
